43e52e28b591f61853002c7149871830b6e0249b
Renkon0705 <liantailang743@gmail.com>
2026-07-27 14:55:13 +0900

    Create xp.js

---
+++ js/xp.js
@@ -0,0 +1,56 @@
+let totalXP =
+Number(localStorage.getItem("totalXP")) || 0;
+
+
+const weakMultiplier = 1.3;
+
+
+function getLevel(xp){
+
+    let level = 1;
+    let need = 100;
+
+    while(xp >= need){
+
+        xp -= need;
+        level++;
+
+        need = Math.floor(need * 1.2);
+
+    }
+
+    return {
+        level:level,
+        current:xp,
+        next:need
+    };
+
+}
+
+
+
+function updateStatus(){
+
+    let data = getLevel(totalXP);
+
+
+    document.getElementById("level").innerHTML =
+    data.level;
+
+
+    document.getElementById("currentXP").innerHTML =
+    data.current;
+
+
+    document.getElementById("nextXP").innerHTML =
+    data.next;
+
+
+    document.getElementById("totalXP").innerHTML =
+    totalXP;
+
+
+    document.getElementById("xpBar").style.width =
+    (data.current / data.next * 100) + "%";
+
+}
